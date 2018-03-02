1e608eed6b478c2595ba3ff5759e78bbd4c64703
Miles Lucas <mdlucas@iastate.edu>
2018-03-02 20:50:03 +0000

    added ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
 
 exec gunicorn kquiz.wsgi:application -w 2 -b :8000
